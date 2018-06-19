cb6fba4bd5c7b829fe7f5cd9f12734f6bce829d6
Deba <aldealdo@gmail.com>
2018-06-19 15:09:10 +0200

    Big Bang

---
+++ public/themes/olivepress/includes/plugins/olive.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+|--------------------------------------------------------------------------
+| Plate
+|--------------------------------------------------------------------------
+|
+| Plate provides a bunch of handy WordPress defaults to help you get the
+| most out of WordPress development.
+|
+| Please see https://github.com/wordplate/plate
+|
+*/
+
+// Disable sidebar menu items.
+add_theme_support('olive-custom-post-types');
