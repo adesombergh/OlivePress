ca8480376af8a945292ccb93c00859d6590bab1f
Aldebaran <aldebaran.desombergh@idweaver.com>
2018-08-27 21:14:49 +0200

    Prevent footer effect too early

--- resources/assets/scripts/app.js
+++ resources/assets/scripts/app.js
@@ -5,21 +5,41 @@ import './booking';
 
 new Vue({
   el: '#app',
+  data: {
+    effectActivated: false,
+    oldOffset: 0,
+    footerHeight: 0,
+    footerFontSize: 14
+  },
   components: {
     'v-nav' : vNav
   },
   methods: {
     handleScroll (event) {
+
       const el = document.scrollingElement || document.documentElement;
       let offset = el.scrollTop + window.innerHeight;
       let height = el.offsetHeight-10;
+      // console.log(offset, height);
+
+      if (offset >= height) {
+        let timeout = setTimeout(() => {
+          clearTimeout(timeout);
+          this.effectActivated = true
+        }, 5000);
+      }
+
       if (offset > height && offset-this.oldOffset > 0) {
+        if (this.effectActivated === false) {
+          return
+        }
         this.footerHeight += 2;
         this.footerFontSize += 2;
       } else if (offset-this.oldOffset < 0) {
         if (this.footerHeight > 10){
           this.footerHeight -= 10;
           this.footerFontSize -= 10;
+          this.effectActivated = false;
         }
       }
       this.oldOffset = offset;
@@ -30,11 +50,6 @@ new Vue({
   },
   destroyed () {
     window.removeEventListener('scroll', this.handleScroll);
-  },
-  data: {
-    oldOffset: 0,
-    footerHeight: 0,
-    footerFontSize: 14
   },
   computed: {
     footerHeightInPixels() {
